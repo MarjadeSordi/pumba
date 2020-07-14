94e906b0d717781017ee3a83070032a2aeb071cb
MarjadeSordi <66893702+MarjadeSordi@users.noreply.github.com>
2020-07-14 20:57:44 -0300

    Add files via upload

---
+++ Templates.js
@@ -0,0 +1,13 @@
+const nome = 'Rebeca'
+const contenacao = 'Olá ' + nome + '!'
+const template = `
+    Olá
+    ${nome}!`
+console.log(contenacao,template)
+
+// expressões
+
+console.log (`1+1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase ()
+console.log(`Ei...${up('cuidado')}!`)
---
+++ organizacao.js
@@ -0,0 +1,6 @@
+console.log('sequencia de código');
+
+{
+    console.log("Olá!");
+    console.log('Mundo!') //Padrãoutilizadonocurso
+}
